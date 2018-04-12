319c2220a6bcb0a9a78830c600175c19b468ec08
Oscar Stickland (OSCAR-PC) <sticosc@donvale.vic.edu.au>
2018-04-12 10:31:54 +1000

    12/4/18 - 10:31

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,24 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    responce = "You're looking at the results of question %s."
+    return HttpResponse(responce % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Your voting on question %s." % question_id)
+
+
